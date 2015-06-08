b1ff66a747f419834be93b4ae1259f4a603fcaf1
Joshua Wood <josh@hintmedia.com>
2015-06-08 15:37:23 -0700

    Add config.

--- honeybadger.go
+++ honeybadger.go
@@ -4,6 +4,24 @@ import (
 	"code.google.com/p/go-uuid/uuid"
 )
 
+type Config struct {
+	APIKey string
+}
+
+var config Config
+
+func Configure(c Config) {
+	if c.APIKey != "" {
+		config.APIKey = c.APIKey
+	}
+}
+
 func Notify(err error) string {
 	return uuid.NewRandom().String()
+}
+
+func init() {
+	config = Config{
+		APIKey: "",
+	}
 }
--- honeybadger_test.go
+++ honeybadger_test.go
@@ -6,11 +6,24 @@ import (
 	"testing"
 )
 
+func TestDefaultConfig(t *testing.T) {
+	if config.APIKey != "" {
+		t.Errorf("Expected config.APIKey to be empty by default. expected=%#v result=%#v", "", config.APIKey)
+	}
+}
+
+func TestConfigure(t *testing.T) {
+	Configure(Config{APIKey: "badgers"})
+	if config.APIKey != "badgers" {
+		t.Errorf("Expected Configure to override config.APIKey. expected=%#v actual=%#v", "badgers", config.APIKey)
+	}
+}
+
 func TestNotifyReturnsUUID(t *testing.T) {
 	err := errors.New("Cobras!")
 	var res string
 	res = Notify(err)
 	if uuid.Parse(res) == nil {
-		t.Errorf("Expected honeybadger.Notify to be a UUID. result=%#v", res)
+		t.Errorf("Expected Notify() to return a UUID. actual=%#v", res)
 	}
 }
