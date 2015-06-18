4131f419509173037d566176c18331401525f7b6
Joshua Wood <josh@hintmedia.com>
2015-06-18 10:54:07 -0700

    Add a better notification test.

--- honeybadger_test.go
+++ honeybadger_test.go
@@ -22,12 +22,20 @@ func TestConfigure(t *testing.T) {
 	}
 }
 
-func TestNotifyReturnsUUID(t *testing.T) {
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(201)
-		fmt.Fprintln(w, "{\"id\":\"87ded4b4-63cc-480a-b50c-8abe1376d972\"}")
-	}))
-	defer ts.Close()
+func TestNotify(t *testing.T) {
+	mux := http.NewServeMux()
+	ts := httptest.NewServer(mux)
+
+	var requests []*http.Request
+	mux.HandleFunc("/v1/notices",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, "POST")
+			requests = append(requests, r)
+			w.WriteHeader(201)
+			fmt.Fprint(w, `{"id":"87ded4b4-63cc-480a-b50c-8abe1376d972"}`)
+		},
+	)
+
 	Configure(Configuration{APIKey: "badgers", Endpoint: ts.URL})
 
 	err := errors.New("Cobras!")
@@ -35,5 +43,19 @@ func TestNotifyReturnsUUID(t *testing.T) {
 	res = Notify(err)
 	if uuid.Parse(res) == nil {
 		t.Errorf("Expected Notify() to return a UUID. actual=%#v", res)
+	}
+
+	Flush()
+
+	if len(requests) != 1 {
+		t.Errorf("Expected 1 request to have been made. actual=%#v", len(requests))
+	}
+}
+
+// Helper functions.
+
+func testMethod(t *testing.T, r *http.Request, method string) {
+	if r.Method != method {
+		t.Errorf("Unexpected request method. actual=%#v expected=%#v", r.Method, method)
 	}
 }
