a04575ad34b0592508a28cf7670a1283bd74af3e
Joshua Wood <josh@hintmedia.com>
2015-06-19 15:00:24 -0700

    Dry up the notify test a bit.

--- honeybadger_test.go
+++ honeybadger_test.go
@@ -11,65 +11,11 @@ import (
 	"testing"
 )
 
-func TestDefaultConfig(t *testing.T) {
-	if Config.APIKey != "" {
-		t.Errorf("Expected config.APIKey to be empty by default. expected=%#v result=%#v", "", Config.APIKey)
-	}
-}
-
-func TestConfigure(t *testing.T) {
-	Configure(Configuration{APIKey: "badgers"})
-	if Config.APIKey != "badgers" {
-		t.Errorf("Expected Configure to override config.APIKey. expected=%#v actual=%#v", "badgers", Config.APIKey)
-	}
-}
-
-func TestNotify(t *testing.T) {
-	mux := http.NewServeMux()
-	ts := httptest.NewServer(mux)
-
-	var requests []*HTTPRequest
-	mux.HandleFunc("/v1/notices",
-		func(w http.ResponseWriter, r *http.Request) {
-			testMethod(t, r, "POST")
-			requests = append(requests, newHTTPRequest(r))
-			w.WriteHeader(201)
-			fmt.Fprint(w, `{"id":"87ded4b4-63cc-480a-b50c-8abe1376d972"}`)
-		},
-	)
-
-	Configure(Configuration{APIKey: "badgers", Endpoint: ts.URL})
-
-	err := errors.New("Cobras!")
-	var res string
-	res = Notify(err)
-	if uuid.Parse(res) == nil {
-		t.Errorf("Expected Notify() to return a UUID. actual=%#v", res)
-	}
-
-	Flush()
-
-	if len(requests) != 1 {
-		t.Errorf("Expected 1 request to have been made. actual=%#v", len(requests))
-	}
-
-	payload := requests[0].decodeJSON()
-
-	if payload["api_key"] != "badgers" {
-		t.Errorf("Expected payload to include API key. expected=%#v actual=%#v", "badgers", payload["api_key"])
-	}
-
-	for _, key := range []string{"notifier", "error", "request", "server"} {
-		switch payload[key].(type) {
-		case map[string]interface{}:
-			// OK
-		default:
-			t.Errorf("Expected payload to include %v hash.", key)
-		}
-	}
-}
-
-// Helper functions.
+var (
+	mux      *http.ServeMux
+	ts       *httptest.Server
+	requests []*HTTPRequest
+)
 
 type HTTPRequest struct {
 	Request *http.Request
@@ -88,6 +34,71 @@ func (h *HTTPRequest) decodeJSON() hash {
 func newHTTPRequest(r *http.Request) *HTTPRequest {
 	body, _ := ioutil.ReadAll(r.Body)
 	return &HTTPRequest{r, body}
+}
+
+func setup(t *testing.T) {
+	mux = http.NewServeMux()
+	ts = httptest.NewServer(mux)
+	requests = []*HTTPRequest{}
+	mux.HandleFunc("/v1/notices",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, "POST")
+			requests = append(requests, newHTTPRequest(r))
+			w.WriteHeader(201)
+			fmt.Fprint(w, `{"id":"87ded4b4-63cc-480a-b50c-8abe1376d972"}`)
+		},
+	)
+
+	client.Config = newConfig(Configuration{APIKey: "badgers", Endpoint: ts.URL})
+}
+
+func TestDefaultConfig(t *testing.T) {
+	if Config.APIKey != "" {
+		t.Errorf("Expected config.APIKey to be empty by default. expected=%#v result=%#v", "", Config.APIKey)
+	}
+}
+
+func TestConfigure(t *testing.T) {
+	Configure(Configuration{APIKey: "badgers"})
+	if Config.APIKey != "badgers" {
+		t.Errorf("Expected Configure to override config.APIKey. expected=%#v actual=%#v", "badgers", Config.APIKey)
+	}
+}
+
+func TestNotify(t *testing.T) {
+	setup(t)
+
+	res := Notify(errors.New("Cobras!"))
+	if uuid.Parse(res) == nil {
+		t.Errorf("Expected Notify() to return a UUID. actual=%#v", res)
+	}
+
+	Flush()
+
+	testRequestCount(t, 1)
+
+	payload := requests[0].decodeJSON()
+
+	testNoticePayload(t, payload)
+}
+
+// Helper functions.
+
+func testRequestCount(t *testing.T, num int) {
+	if len(requests) != num {
+		t.Errorf("Expected %v request to have been made. expected=%#v actual=%#v", num, num, len(requests))
+	}
+}
+
+func testNoticePayload(t *testing.T, payload hash) {
+	for _, key := range []string{"notifier", "error", "request", "server"} {
+		switch payload[key].(type) {
+		case map[string]interface{}:
+			// OK
+		default:
+			t.Errorf("Expected payload to include %v hash.", key)
+		}
+	}
 }
 
 func testMethod(t *testing.T, r *http.Request, method string) {
