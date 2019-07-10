75c802252878614c525723f2db8ff6bab056faa6
Chris Gaffney <gaffneyc@gmail.com>
2019-07-10 11:32:10 -0400

    Fixes stack traces when methods are inlined
    Go introduced runtime.CallerFrames in 1.7 which can expand frames for
    inlined methods[1]. They're recommending all code switch to use it.
    
    This really came up because the TestNewErrorTrace was failing on Go
    1.12.6 since at some point they added in a line for the method defined
    in TestNewErrorTrace. I've expanded the test to make sure the top two
    lines are from the code that is calling NewError and doesn't include a
    reference to the library itself.
    
    1: https://golang.org/doc/go1.12#compiler

--- error_test.go
+++ error_test.go
@@ -11,12 +11,32 @@ func TestNewErrorTrace(t *testing.T) {
 	}
 
 	err := fn()
+
+	// The stack should look like this:
+	//   github.com/honeybadger-io/honeybadger-go.TestNewErrorTrace.func1
+	//   github.com/honeybadger-io/honeybadger-go.TestNewErrorTrace
+	//   testing.tRunner
+	//   runtime.goexit
 	if len(err.Stack) < 3 {
 		t.Errorf("Expected to generate full trace")
 	}
 
-	method := strings.Split(err.Stack[1].Method, ".")
-	if method[len(method)-1] != "TestNewErrorTrace" {
-		t.Errorf("Expected to generate a proper trace")
+	// Checks that the top top methods are the (inlined) fn and the test Method
+	expected := []string{
+		".TestNewErrorTrace.func1",
+		".TestNewErrorTrace",
+	}
+
+	for i, suffix := range expected {
+		method := err.Stack[i].Method
+
+		if !strings.HasSuffix(method, suffix) {
+			// Logs the stack to give some context about the error
+			for j, stack := range err.Stack {
+				t.Logf("%d: %s", j, stack.Method)
+			}
+
+			t.Fatalf("stack[%d].Method expected_suffix=%q actual=%q", i, suffix, method)
+		}
 	}
 }
